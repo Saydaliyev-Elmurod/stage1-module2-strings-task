df74d6c28b165fbc7950ba62eec10cad8234a7bf
saydaliyev elmurod <saydaliyevelmurod01@gmail.com>
2024-03-27 17:59:57 +0500

    first method finished

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,5 +1,6 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -8,11 +9,26 @@ public class StringSplitter {
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> response = new ArrayList<>();
+        char[] characters = source.toCharArray();
+        StringBuilder text = new StringBuilder();
+        for (char character : characters) {
+            if (delimiters.contains(character + "")) {
+                response.add(text.toString());
+                text = new StringBuilder();
+            } else {
+                text.append(character);
+            }
+        }
+        if (text.length() != 0) {
+            response.add(text.toString());
+        }
+        return response;
+
     }
 }
